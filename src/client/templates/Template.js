9dc3cf642fb5ec10caa1a0cdcdf2fdf59f70c325
Limoer96 <myt19960503@outlook.com>
2023-04-28 15:17:56 +0800

    feat: 更新模版

--- src/client/templates/Template.js
+++ src/client/templates/Template.js
@@ -1,24 +1,47 @@
-// const Template = require("sv-builder-template");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const Template = require('sv-builder-template')
 
-// class TemplateClass extends Template {
-//   // 必须要实现的方法
-//   getTemplateContent(doc) {
-//     const [queryParams, bodyParams] = this.getTypeParams();
-//     return `
-//       import request from '@/utils/request'
+class TemplateClass extends Template {
+  // 必须要实现的方法
+  // eslint-disable-next-line prettier/prettier, @typescript-eslint/explicit-member-accessibility
+  getTemplateContent(doc) {
+    const { queryParams, bodyParams, pathParams } = this.getTypeParams()
+    const usedParams = ['query']
+    if (bodyParams) {
+      usedParams.push('body')
+    }
+    if (pathParams && pathParams.length) {
+      usedParams.push('path')
+    }
+    const usedParamsStr = `{${usedParams.join(', ')}}`
+    return `
+      import request from '@/utils/request'
+      import { stringify } from 'qs'
+      type BodyParams = ${bodyParams || 'unknown'}
+      
+      interface QueryParams {
+        ${this.getTypeQueryParams(queryParams)}
+      }
+      interface PathParams {
+        ${this.getTypeQueryParams(pathParams)}
+      }
+      interface IParams {
+        query: QueryParams
+        body: BodyParams
+        path: PathParams
+      }
+      /**
+       * ${doc.summary}
+       */
+      export function ${doc.fileName}(${usedParamsStr}: IParams) {
+        return request<${this.getResponseType()}>({
+          url: \`${this.getUrl()}?\${stringify(query||{}, {arrayFormat: 'repeat'})}\`,
+          method: '${doc.method}',
+          ${bodyParams ? 'body' : ''}
+        })
+      }
+    `
+  }
+}
 
-//       type BodyParams = ${bodyParams}
-//       interface QueryParams {
-//         ${this.getTypeQueryParams(queryParams)}
-//       }
-//       /**
-//        * ${doc.summary}
-//        */
-//       export function ${doc.fileName}(query: QueryParams, body: BodyParams) {
-//         return request('${doc.url}', '${doc.method}', query, body)
-//       }
-//     `;
-//   }
-// }
-
-// module.exports = TemplateClass;
+module.exports = TemplateClass
